b4a3f457a9806635528e993d980f8f478db741aa
Matt Lehrer <lehrerm@gmail.com>
2023-08-01 21:32:29 +0200

    line with color

--- src/lib/Lorenz.svelte
+++ src/lib/Lorenz.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
 	import { T, useFrame } from '@threlte/core';
-	import { BufferGeometry, Float32BufferAttribute, PointsMaterial } from 'three';
+	import {
+		BufferAttribute,
+		BufferGeometry,
+		Float32BufferAttribute,
+		LineBasicMaterial,
+		PointsMaterial,
+		Color
+	} from 'three';
 	import { Grid, OrbitControls } from '@threlte/extras';
 
-	const geometry = new BufferGeometry();
+	const MAX_POINTS = 25000;
 
-	const material = new PointsMaterial({
-		// transparent: true,
+	const dotGeometry = new BufferGeometry();
+	const lineGeometry = new BufferGeometry();
+
+	const dotMaterial = new PointsMaterial({
+		// transparent: true
 		sizeAttenuation: false
 	});
+	const lineMaterial = new LineBasicMaterial();
 
-	let color = [255, 255, 255] as [number, number, number];
+	let dotColor = new Color(0xff2211);
+	let lineColor = new Color(0x997777);
 	let opacity = 1;
 	let size = 3;
 
@@ -23,6 +35,10 @@
 	let c = 8.0 / 3.0;
 
 	const dt = 0.01;
+	let i = 0;
+
+	const positions = new Float32Array(MAX_POINTS * 3); // 3 vertices per point
+	lineGeometry.setAttribute('position', new BufferAttribute(positions, 3));
 
 	useFrame(() => {
 		let dx = a * (y - x) * dt;
@@ -32,7 +48,15 @@
 		y = y + dy;
 		z = z + dz;
 		// console.log({ x, y, z });
-		geometry.setAttribute('position', new Float32BufferAttribute([x, y, z], 3));
+
+		const positions = lineGeometry.getAttribute('position').array;
+		positions[i++] = x;
+		positions[i++] = y;
+		positions[i++] = z;
+
+		dotGeometry.setAttribute('position', new Float32BufferAttribute([x, y, z], 3));
+		lineGeometry.setAttribute('position', new BufferAttribute(positions, 3));
+		lineGeometry.setDrawRange(0, i / 3);
 	});
 </script>
 
@@ -41,7 +65,7 @@
 		autoRotate
 		enableZoom={true}
 		enableDamping
-		autoRotateSpeed={0}
+		autoRotateSpeed={0.5}
 		target.y={0}
 		target.z={15}
 	/>
@@ -49,6 +73,16 @@
 
 <T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
 <T.AmbientLight intensity={0.2} />
+
+<T.Points>
+	<T is={dotGeometry} />
+	<T is={dotMaterial} {size} color={dotColor} {opacity} />
+</T.Points>
+
+<T.Line>
+	<T is={lineGeometry} />
+	<T is={lineMaterial} color={lineColor} opacity={0.05} />
+</T.Line>
 
 <!-- <Grid
 	position.y={-0.001}
@@ -59,8 +93,3 @@
 	cellSize={10}
 	gridSize={100}
 /> -->
-
-<T.Points>
-	<T is={geometry} />
-	<T is={material} {size} {color} {opacity} />
-</T.Points>
