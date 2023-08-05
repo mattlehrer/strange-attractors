4a22f59250cba404d9ddfe6a518a5f6aaae95ca6
Matt Lehrer <lehrerm@gmail.com>
2023-08-05 12:17:45 +0200

    fix mobile safari

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,16 @@
 	import App from '$lib/App.svelte';
 </script>
 
+<svelte:head>
+	<meta name="theme-color" content="#0d1320" />
+	<meta name="description" content="A 3D Lorenz Attractor built with Svelte and Three.js" />
+	<!-- disable zoom -->
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+	/>
+</svelte:head>
+
 <div>
 	<App />
 </div>
@@ -15,7 +25,7 @@
 		position: relative;
 		width: 100vw;
 		height: 100vh;
-		background: rgb(13, 19, 32);
+		background: #0d1320;
 		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
 	}
 </style>
