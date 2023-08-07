2473ef018535f1673867fd82f8d629ca0175fe5c
Matt Lehrer <lehrerm@gmail.com>
2023-08-07 10:39:14 +0200

    add 5 dots button

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -8,18 +8,26 @@
 	const controls = knobby.panel({
 		$id: 'main',
 		autorotate: true,
-		'Add Dot': () => {
-			const newDot = {
-				name: `Dot ${$lorenzPositions.length + 1}`,
-				dotColor: '#ff2211',
-				x: Number(Math.random().toFixed(10)),
-				y: Number(Math.random().toFixed(10)),
-				z: Number(Math.random().toFixed(10))
-			};
-			$lorenzPositions = [...$lorenzPositions, newDot];
+		'Add Dot': addDot,
+		'Add 5 Dots': () => {
+			for (let i = 0; i < 5; i++) {
+				addDot();
+			}
 		}
 	});
+
 	$controls.message = $controls.message;
+
+	function addDot() {
+		const newDot = {
+			name: `Dot ${$lorenzPositions.length + 1}`,
+			dotColor: '#ff2211',
+			x: Number(Math.random().toFixed(10)),
+			y: Number(Math.random().toFixed(10)),
+			z: Number(Math.random().toFixed(10))
+		};
+		$lorenzPositions = [...$lorenzPositions, newDot];
+	}
 </script>
 
 <T.PerspectiveCamera makeDefault position={[-150, 150, 150]} fov={35}>
